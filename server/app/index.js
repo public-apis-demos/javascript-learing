30a8675159c05f9f4d9dd8565467f9eed6d41822
kasiriveni <kasiriveni@gmail.com>
2019-05-23 08:08:25 +0530

    adding the server code

---
+++ server/app/index.js
@@ -0,0 +1,6 @@
+// var { name, c, getUser } = require("./person");
+import { name, c, getUser } from "./person";
+
+console.log(name, c);
+
+console.log(getUser());
---
+++ server/app/person.js
@@ -0,0 +1,15 @@
+import fs from "fs";
+import path from "path";
+
+export let name = "srinivas";
+export const c = 1;
+
+export let getUser = () => "hello world";
+
+// module.exports = {
+//   name,
+//   c,
+//   getUser
+// };
+
+console.log(fs, path);

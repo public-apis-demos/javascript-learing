dacc98bb2addb12acac73f6418de9b04797ecea7
kasiriveni <kasiriveni@gmail.com>
2019-06-05 22:52:54 +0530

    update the fiels

---
+++ setup/learn-testing/learn-jasmine/src/index.js
@@ -0,0 +1,11 @@
+function Add(a,b){
+  return a+b;
+}
+
+Add(1,2)  //3
+
+Add(-1,3) // 2
+
+Add(-1,-1) //-2
+
+Add(1,-2)  // -1
---
+++ setup/learn-testing/learn-jasmine/src/index.spec.js
@@ -0,0 +1,21 @@
+describe("Testing the Add method",function(){
+  var a=1,b=2;
+
+it("adding the pos nbr",function(){
+  expect(a).to
+})
+
+
+it("adding the neg nbr",function(){
+
+})
+
+it("adding the both nbr",function(){
+
+})
+
+it("adding the + - nbr",function(){
+
+})
+
+})

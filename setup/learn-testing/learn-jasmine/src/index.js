939daf5ca88fa8680001f3d90811e72c34108ec5
kasiriveni <kasiriveni@gmail.com>
2019-06-06 07:34:22 +0530

    init

--- setup/learn-testing/learn-jasmine/src/index.js
+++ setup/learn-testing/learn-jasmine/src/index.js
@@ -1,11 +1,3 @@
 function Add(a,b){
   return a+b;
 }
-
-Add(1,2)  //3
-
-Add(-1,3) // 2
-
-Add(-1,-1) //-2
-
-Add(1,-2)  // -1
--- setup/learn-testing/learn-jasmine/src/index.spec.js
+++ setup/learn-testing/learn-jasmine/src/index.spec.js
@@ -1,4 +1,7 @@
-describe("Testing the Add method",function(){
+import Hello from "./hello"
+
+
+xdescribe("Testing the Add method",function(){
   var a=1,b=2;
 
 it("adding the pos nbr",function(){
@@ -18,4 +21,29 @@ it("adding the + - nbr",function(){
 
 })
 
+})
+
+
+xdescribe("Mult of method",()=>{
+   let name="srinivas"
+  xit("adding the mult",()=>{
+    expect(name).toBe("sdfasdfasdf")
+  })
+
+  fit("adding the mult",()=>{
+    expect(name).toBe("srinivas")
+  })
+
+  it("2",()=>{
+    expect(name).toBe("srinivas")
+  })
+})
+
+
+describe("Checking the Hello method",()=>{
+
+  var rest =Hello();
+  it("it reutnr empty",()=>{
+    expect(res).nothing()
+  })
 })
